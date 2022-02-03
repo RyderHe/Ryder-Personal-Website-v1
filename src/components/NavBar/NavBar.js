0dd94a4427e3a753fcad7ececc19d62a9386b454
Ryder He <r37he@edu.uwaterloo.ca>
2022-02-03 10:51:42 -0500

    Add responsiveness to Home page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,20 +7,9 @@ const NavBar = () => {
 
   return (
     <nav className={location.pathname !== "/" ?  "opaque" : ""}>
-    
-      {/* <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a> */}
-
-      {/* <ul id="nav" className="nav">
-        <li key="1"><a href="#home" key="1" onClick={onClickLink}>Home</a></li>
-        <li key="2"><a href="#about" onClick={onClickLink}>About</a></li>
-        <li key="3"><a href="#resume" onClick={onClickLink}>Resume</a></li>
-        <li key="4"><a href="#portfolio" onClick={onClickLink}>Works</a></li>
-      </ul> */}
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/resume">Resume</NavLink>
-
     </nav>
   )
 }

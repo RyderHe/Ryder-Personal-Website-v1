a71d5e05d8f900446d3ff7c01362f3dd86329c2e
Ryder He <r37he@edu.uwaterloo.ca>
2022-02-03 23:06:14 -0500

    Componentized projects sections

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,20 @@
+const ProjectCard = (props) => {
+  const { projectData } = props;
+  return (
+    <div className="projects_item">
+      <a href={projectData.url} title={projectData.title} target="_blank"  rel="noreferrer">
+        <img alt={projectData.title} src={`${process.env.PUBLIC_URL}/images/${projectData.image}`} />
+        <div className="projects_item__overlay___wrapper">
+          <h5>{projectData.title}</h5>
+          <p>{projectData.category}</p>
+          <div className="used-skills">
+            {projectData.skills.map((skill, index) => <p key={index}>{skill}</p>)}
+          </div>
+          <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export default ProjectCard;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,8 @@
 import "./Projects.css";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
 const Projects = (props) => {
-  // const { projectsData } = props;
+  const { projectsData } = props;
 
   return (
     <div id="projects">
@@ -9,12 +10,12 @@ const Projects = (props) => {
       <h1 className="projects_header">Check Out Some of My Projects.</h1>
         
       <div className="projects_container">
+        { projectsData.map((project, index) => <ProjectCard key={index} projectData={project} />)}
 
-        <div key={1} className="projects_item">
+        {/* <div key={1} className="projects_item">
           <a href="https://github.com/RyderHe" title={"projects.title"} target="_blank"  rel="noreferrer">
             <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
             <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
               <h5>Chat App</h5>
               <p>A clone game - Fly the &#39;spyderman&#39; as far as you can!</p>
               <div className="used-skills">
@@ -27,14 +28,12 @@ const Projects = (props) => {
               </div>
               <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
             </div>
-            {/* </div>  */}
           </a>
         </div>
         <div key={1} className="projects_item">
           <a href="https://github.com/RyderHe" title={"projects.title"} target="_blank"  rel="noreferrer">
             <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
             <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
               <h5>Ludum Dare Game Prediction Kaggle Challenge</h5>
               <p>Predicted with a categorization accuracy of 0.92+ that ranked top 50% among the class competition.</p>
               <div className="used-skills">
@@ -43,83 +42,10 @@ const Projects = (props) => {
                 <p>Vue</p>
 
               </div>
-              {/* </div> */}
               <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
             </div> 
           </a>
-        </div>
-        <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
-            <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
-            <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
-              <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
-              <div className="used-skills">
-                <p>Javascript</p>
-                <p>PHP</p>
-                <p>Vue</p>
-
-              </div>
-              {/* </div> */}
-              <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
-            </div> 
-          </a>
-        </div>
-        <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
-            <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
-            <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
-              <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
-              <div className="used-skills">
-                <p>Javascript</p>
-                <p>PHP</p>
-                <p>Vue</p>
-
-              </div>
-              {/* </div> */}
-              <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
-            </div> 
-          </a>
-        </div>
-        <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
-            <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
-            <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
-              <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
-              <div className="used-skills">
-                <p>Javascript</p>
-                <p>PHP</p>
-                <p>Vue</p>
-
-              </div>
-              {/* </div> */}
-              <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
-            </div> 
-          </a>
-        </div>
-        <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
-            <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
-            <div className="projects_item__overlay___wrapper">
-              {/* <div className="projects_item__overlay___meta"> */}
-              <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
-              <div className="used-skills">
-                <p>Javascript</p>
-                <p>PHP</p>
-                <p>Vue</p>
-
-              </div>
-              {/* </div> */}
-              <div className="projects_item__link-icon"><i className="fa fa-link"></i></div>
-            </div> 
-          </a>
-        </div>
+        </div> */}
        
 
       </div>

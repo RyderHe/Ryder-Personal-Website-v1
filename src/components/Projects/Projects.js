219ae9e4a16cd74c7fd98b013054066fc1c0c763
Ryder He <r37he@edu.uwaterloo.ca>
2022-02-03 22:54:47 -0500

    Add responsiveness to project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import "./Projects.css";
 
 const Projects = (props) => {
+  // const { projectsData } = props;
+
   return (
     <div id="projects">
 
@@ -9,12 +11,12 @@ const Projects = (props) => {
       <div className="projects_container">
 
         <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
+          <a href="https://github.com/RyderHe" title={"projects.title"} target="_blank"  rel="noreferrer">
             <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
             <div className="projects_item__overlay___wrapper">
               {/* <div className="projects_item__overlay___meta"> */}
               <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
+              <p>A clone game - Fly the &#39;spyderman&#39; as far as you can!</p>
               <div className="used-skills">
                 <p>Javascript</p>
                 <p>PHP</p>
@@ -29,12 +31,12 @@ const Projects = (props) => {
           </a>
         </div>
         <div key={1} className="projects_item">
-          <a href="https://github.com/RyderHe" title={"projects.title"}>
+          <a href="https://github.com/RyderHe" title={"projects.title"} target="_blank"  rel="noreferrer">
             <img alt={"projects.title"} src={`${process.env.PUBLIC_URL}/images/profile_photo.jpg`} />
             <div className="projects_item__overlay___wrapper">
               {/* <div className="projects_item__overlay___meta"> */}
-              <h5>Chat App</h5>
-              {/* <p>projects.category</p> */}
+              <h5>Ludum Dare Game Prediction Kaggle Challenge</h5>
+              <p>Predicted with a categorization accuracy of 0.92+ that ranked top 50% among the class competition.</p>
               <div className="used-skills">
                 <p>Javascript</p>
                 <p>PHP</p>
